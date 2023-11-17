5cd08db3d9b4c2c7af096f1508374abeeccb3415
Krishna-nettem <142159309+Krishna-nettem@users.noreply.github.com>
2023-11-17 18:36:27 +0000

    made a lot of changes, changed the background image and added different html files

--- app.py
+++ app.py
@@ -3,9 +3,27 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-
 def index() :
+  return render_template("index.html")
+
+
+
+@app.route("/home")
+def home() :
   return render_template("home.html")
+
+@app.route('/artists')
+def artists():
+  return render_template("artists.html")
+
+@app.route('/playlists')
+def playlists():
+  return render_template("playlists.html")
+
+@app.route('/settings')
+def settings():
+  return render_template("settings.html")
+    
 
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', debug = True)
